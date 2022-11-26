1f5d3dd5f61d5d15f622da1aa337eb96bb3e5cb9
jinmu333 <dreamhunter2333@gmail.com>
2022-11-26 17:06:13 +0800

    fead: add keymap json

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 ## 使用
 
+keymap.json 为琴键映射
+
 ```bash
 python3 -m venv ./venv
 ./venv/bin/python3 -m pip install -r requirements.txt
-./venv/bin/python3 main.py -f 打上花火.mid -b 120 -w 5
+./venv/bin/python3 main.py -f 打上花火.mid -b 120 -w 5 --keymap_file keymap.json
 ```
 
 ## 参考
 
-[https://github.com/WitchElaina/Genshin_Zither_Player](https://github.com/WitchElaina/Genshin_Zither_Player)
+- [https://github.com/WitchElaina/Genshin_Zither_Player](https://github.com/WitchElaina/Genshin_Zither_Player)
+- [https://github.com/YidaozhanYa/GenshinMIDIPlay-Linux](https://github.com/YidaozhanYa/GenshinMIDIPlay-Linux)
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import time
+from typing import Dict
 import mido
 import click
 import logging
 import pyautogui
+
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    keymap: Dict[int, str]
+
 
 KEY_MAP = {
     60: 'z', 62: 'x', 64: 'c', 65: 'v', 67: 'b', 69: 'n', 71: 'm',
@@ -20,36 +28,7 @@ ch.setFormatter(formatter)
 _logger.addHandler(ch)
 
 
-def allToCMajor(file_name, force):
-    note_temp = set(
-        msg.note
-        for msg in mido.MidiFile(file_name)
-        if msg.type == "note_on"
-    )
-    res = [
-        i
-        for i in range(-48, 48)
-        if not any((int(cur)+i) not in KEY_MAP for cur in note_temp)
-    ]
-    if not res and force:
-        max_common = max(
-            [
-                (i, sum((int(cur)+i) in KEY_MAP for cur in note_temp))
-                for i in range(-48, 48)
-            ],
-            key=lambda t: t[1]
-        )
-        note_temp = [
-            int(int(cur)+max_common[0]) for cur in note_temp
-        ]
-        _logger.info(
-            f"max_common={max_common} note_temp={note_temp}"
-        )
-        res = [max_common[0]]
-    return res
-
-
-def get_tempo(mid):
+def get_tempo(mid: mido.MidiFile) -> int:
     for track in mid.tracks:
         for msg in track:
             if msg.type == 'set_tempo':
@@ -58,7 +37,7 @@ def get_tempo(mid):
         return mido.midifiles.midifiles.DEFAULT_TEMPO
 
 
-def playMidi(file_name, m_bpm, m_key_add):
+def playMidi(file_name: str, m_bpm: int, m_key_add: int, keymap: Dict[int, str]) -> None:
     midi = mido.MidiFile(file_name)
     bpm = m_bpm or mido.tempo2bpm(get_tempo(midi))
     real_time = float(120 / bpm)
@@ -67,11 +46,11 @@ def playMidi(file_name, m_bpm, m_key_add):
     )
     # Read midi file
     for msg in midi:
-        if msg.type == "note_on" and KEY_MAP.get(int(msg.note)+m_key_add):
+        if msg.type == "note_on" and keymap.get(int(msg.note)+m_key_add):
             sleep_time = float(msg.time) * real_time
             pyautogui.sleep(sleep_time)
-            pyautogui.keyDown(KEY_MAP.get(int(msg.note)+m_key_add))
-            pyautogui.keyUp(KEY_MAP.get(int(msg.note)+m_key_add))
+            pyautogui.keyDown(keymap.get(int(msg.note)+m_key_add))
+            pyautogui.keyUp(keymap.get(int(msg.note)+m_key_add))
         elif msg.type == "note_on":
             _logger.info(
                 f"Skip {msg.note} {m_key_add}"
@@ -84,24 +63,40 @@ def playMidi(file_name, m_bpm, m_key_add):
 @ click.command()
 @ click.option('--file_name', '-f', 'file_name', required=True, type=str)
 @ click.option('--bpm', '-b', 'bpm', default=0, required=True, type=int)
-@ click.option('--force', '-force', 'force', default=False, required=True, type=bool)
 @ click.option('--wait_seconds', '-w', 'wait_seconds', default=5, required=True, type=int)
-def main(file_name: str, force: bool, bpm: int, wait_seconds: int) -> None:
+@ click.option('--keymap_file', '-k', 'keymap_file', default="keymap.json", type=str)
+def main(file_name: str, bpm: int, wait_seconds: int, keymap_file: str) -> None:
     _logger.info(
-        f"Start file_name={file_name}, bpm={bpm}, wait_seconds={wait_seconds}"
+        f"Start file_name={file_name}, bpm={bpm}, wait_seconds={wait_seconds}, keymap_file={keymap_file}"
     )
 
+    keymap = Settings.parse_file(
+        keymap_file
+    ).keymap if keymap_file else KEY_MAP
+
+    # 调整 key
     note_add = 0
-    key_add = allToCMajor(file_name, force)
+    note_temp = set(
+        msg.note
+        for msg in mido.MidiFile(file_name)
+        if msg.type == "note_on"
+    )
+    key_add = [
+        i
+        for i in range(-48, 48)
+        if not any((int(cur)+i) not in keymap for cur in note_temp)
+    ]
     if not key_add:
         _logger.error("The midi file is too complex to play!")
         return
-    _logger.info(f"key add values: {key_add}")
-    note_add = int(input("请输入 key add value: "))
+
+    if note_add not in key_add:
+        _logger.info(f"key add values: {key_add}")
+        note_add = int(input("请输入 key add value: "))
 
     _logger.info("5s 后开始播放")
     time.sleep(5)
-    playMidi(file_name, bpm, note_add)
+    playMidi(file_name, bpm, note_add, keymap)
     _logger.info("Done.")
 
 
